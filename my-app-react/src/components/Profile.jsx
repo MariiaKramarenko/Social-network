b13e76f274378b5cc7962a6209279c4026e9cd36
Mariia Kramarenko <mariya.kramarenko00@gmail.com>
2019-11-09 19:16:35 +0100

    CREATED SIMPLE COMPONENTS FOR MY APP

---
+++ my-app-react/src/components/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './Profile.module.css';
+
+
+const Profile = () => {
+   
+return (
+   <div className={s.content}>
+   	
+   </div>
+	)
+}
+
+export default Profile;
