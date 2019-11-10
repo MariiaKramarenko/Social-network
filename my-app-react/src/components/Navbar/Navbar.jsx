61688db7fd8ee29eafbc7532aedc83ffade2a5a1
Mariia Kramarenko <mariya.kramarenko00@gmail.com>
2019-11-10 14:28:28 +0100

    addet NavLink , activeClassName for css styling

--- my-app-react/src/components/Navbar/Navbar.jsx
+++ my-app-react/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import s from './Navbar.module.css';
-
+import {NavLink} from 'react-router-dom';
 
 
 const Navbar = () => {
-	return (
+  return (
           <nav className={s.nav}>
              <div className={s.item}>
-                   <a href="/profile">Profile</a>
+                   <NavLink to="/profile" activeClassName={s.activeLink}>Profile</NavLink>
              </div>
-             <div className={ `${s.item} ${s.active}`}>
-                   <a href="/dialogs" >Messages</a>
+             <div className={ `${s.item} ${s.active}` }>
+                   <NavLink to="/dialogs" activeClassName={s.activeLink}>Messages</NavLink>
              </div>
              <div className={s.item}>
                    <a href="#">News</a>
@@ -23,7 +23,7 @@ const Navbar = () => {
              </div>
          </nav>
 
-		)
+    )
 }
 
 export default Navbar;
