6ed4db5b1489088e7c9aaf5be35a9112c6747e1e
Mariia Kramarenko <mariya.kramarenko00@gmail.com>
2019-11-10 15:35:51 +0100

    added NavLink fordialogs, updated component Dialogs.jsx

--- my-app-react/src/components/Dialogs/Dialogs.jsx
+++ my-app-react/src/components/Dialogs/Dialogs.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import s from './Dialogs.module.css';
-
+import {NavLink} from 'react-router-dom';
 
 const Dialogs = (props) => {
 	return(
-		<div>
-		Dialogs
+	
+		<div className={s.dialogs}>
+			<div className={s.dialogsItem}> 
+				<div className={s.dialog}>
+					<NavLink to="/dialogs/1">Mariia </NavLink>
+				</div>
+				<div className={s.dialog}>
+					<NavLink to="/dialogs/2">Arek </NavLink>
+				</div>
+				<div className={s.dialog}>
+					<NavLink to="/dialogs/3">Andrey </NavLink>
+				</div>
+			</div>
+		<div className={s.messages}>
+          <div className={s.message}> Hi!</div>
+          <div className={s.message}> Hello!</div>
+          <div className={s.message}> Good day!</div>
 		</div>
+		</div>
+
+		
 		)
 }
 
