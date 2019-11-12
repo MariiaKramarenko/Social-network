c1e4668c7b7ec0b27f2a9b5fb9c58d04cae00229
Mariia Kramarenko <mariya.kramarenko00@gmail.com>
2019-11-12 14:34:16 +0100

    added ref for button in Dialogs

--- my-app-react/src/components/Dialogs/Dialogs.jsx
+++ my-app-react/src/components/Dialogs/Dialogs.jsx
@@ -11,7 +11,13 @@ const Dialogs = (props) => {
 
     let massagesElements = props.state.messages.map( m =>
     	<Message message ={m.message} />);
-
+    
+    let newMessageElement = React.createRef();
+   
+    let addMessage =() =>{
+    let text =newMessageElement.current.value;
+    alert (text);
+    }
 	return(	   
 		<div className={s.dialogs}>
 			<div className={s.dialogsItems}> 				
@@ -19,7 +25,9 @@ const Dialogs = (props) => {
 			</div>
 		<div className={s.messages}>         
           {massagesElements}
+          <textarea ref={newMessageElement}></textarea> <button onClick={addMessage}>Send message</button>
 		</div>
+
 		</div>		
 		)
 }
