1e2046574f559eef8c5e14d56e10c9b6796ec3dc
Mariia Kramarenko <mariya.kramarenko00@gmail.com>
2019-12-07 18:13:22 +0100

    added button for show all users

--- my-app-react/src/components/Users/Users.jsx
+++ my-app-react/src/components/Users/Users.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import s from './users.module.css';
 import * as axios from 'axios';
-
+import userPhoto from '../../../src/assets/images/User.png';
 
 let Users = (props) => {
-
-  if (props.users.length === 0) {
+  let getUsers = () => {
+   if (props.users.length === 0) {
 
     axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response=> {/*в response приходит ответ от сервера */
       props.setUsers(response.data.items)/*смотри через дебаг что приходит в респонс и оотуда вытягиваем-а теперь мы их берем этих юзеров с сервера и сетаем(вставляем) в наш стейт!!*/
-    });/*data.items-это наши юзеры*/
-
-
+    });/*data.items-это наши юзеры*/}
   }
+
 	return  <div>
+     <button onClick={getUsers}> Get users </button>
       {
       	props.users.map(u => <div key = {u.id} > 
              <span>
              	<div>
-             		<img src={u.photoUrl} className={s.photo}/>
+             		<img src={u.photos.small != null ? u.photos.small : userPhoto} className={s.photo}/>
              	</div>
              	<div>
              		{u.followed 
