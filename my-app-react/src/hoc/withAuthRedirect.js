96a114a3deb19b9b0a0d12a09640d5bb001878da
Mariia Kramarenko <mariya.kramarenko00@gmail.com>
2020-01-04 17:25:26 +0100

    created HOC /*comment inside */

---
+++ my-app-react/src/hoc/withAuthRedirect.js
@@ -0,0 +1,13 @@
+import React from 'react';
+/*суть хока-возвращаем один и тот же хок для разных компонент которые приходят в него как целевые*/
+export const withAuthRedirect =(Component)=>{/*функция кот принимает в себя компоненту кот будет отрисована*/
+     class RedirectComponent extends React.Component {/*создаем внутри класс */
+     	render(){/*внутри класса делаем логику редиректа*/
+     		if(!this.props.isAuth) return <Redirect to='/login' />/*если в пропсах isAuth равен false то ретурим 
+     			редирект компоненту Redirect на страницу login*/
+
+     		return <Component {...this.props}/*отрисовывание компоненты и прокидывание в нее пропсов*/
+     	}
+     }
+     return RedirectComponent;
+}
