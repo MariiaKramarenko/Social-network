769967fae691f34b64ea318ba49364c9bbd01f13
Mariia Kramarenko <mariya.kramarenko00@gmail.com>
2020-01-05 18:53:10 +0100

    used compose()(Dialogs) in DialogsContainer

--- my-app-react/src/components/Dialogs/DialogsContainer.jsx
+++ my-app-react/src/components/Dialogs/DialogsContainer.jsx
@@ -7,7 +7,7 @@ import {sendMessageCreator, updateNewMessageBodyCreator} from '../../redux/dialo
 import Dialogs from './Dialogs';
 import {connect} from 'react-redux';
 import {withAuthRedirect} from '../../hoc/withAuthRedirect';
-
+import {compose} from 'redux';
 
 /*этими двумя функциями мы настраиваем наш connect */
 let mapStateToProps =(state)=>{/* тут присваиваем свойства ,цель-превратить часть стейта в пропсы для компоненты*/
@@ -30,12 +30,19 @@ let mapDispatchToProps = (dispatch) => {/*тут передадим коллбе
 }
 
 
-let AuthRedirectComponent = withAuthRedirect(Dialogs);/*оборачиваем в хок компоненту Dialogs в хоку будет происходить редирект
+
+
+
+/*закомментированная логика ниже заменена на функцию compose*/
+
+
+
+/*let AuthRedirectComponent = withAuthRedirect(Dialogs);/*оборачиваем в хок компоненту Dialogs в хоку будет происходить редирект
 на логин если isAuth:false*/
 
 
 /*connect-возвращает нам новую контейнерную компоненту*/
-const DialogsContainer = connect (mapStateToProps,mapDispatchToProps)(AuthRedirectComponent);/*вызываем функцию два раза она создает контейнерную компоненту 
+/*const DialogsContainer = connect(mapStateToProps,mapDispatchToProps)(AuthRedirectComponent);/*вызываем функцию два раза она создает контейнерную компоненту 
 и внутри вызывает переданную ей во вторых скобках-презентационную*/
 /*библиотека connect вернет нам в этом случае функциональную компоненту для нашей презентационной Dialogs
 то есть нам уже не нужно создавать контейнерные компоненты вручную,за нас это сделает connect
@@ -47,4 +54,7 @@ const DialogsContainer = connect (mapStateToProps,mapDispatchToProps)(AuthRedire
 */
 
 
-export default DialogsContainer;
+export default compose(
+  connect(mapStateToProps,mapDispatchToProps),
+  withAuthRedirect
+  )(Dialogs);/*функция компоуз-берет Dialogs закидывает в withAuthRedirect и полученный результат закидывает в connect*/
