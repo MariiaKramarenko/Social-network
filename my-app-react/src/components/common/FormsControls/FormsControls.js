7587788186c895412c2f48e160bf91fc3f027b21
Mariia Kramarenko <mariya.kramarenko00@gmail.com>
2020-01-10 13:48:48 +0100

    refactoring in FormsControl (deleted dublicate code)

--- my-app-react/src/components/common/FormsControls/FormsControls.js
+++ my-app-react/src/components/common/FormsControls/FormsControls.js
@@ -1,32 +1,30 @@
 import React from 'react';
 import styles from './FormControls.module.css'
 
-export const Textarea = ({input, meta, ...props})=>{
-	
-    const hasError = meta.touched && meta.error; /*это приходит из пропсов валилатора,см документацию*/
+
+export const FormControl = ({input, meta, child, ...props}) => {
+      const hasError = meta.touched && meta.error; /*это приходит из пропсов валилатора,см документацию*/
     /*если есть ошибка в ответа пропсов,то покажем красный спан надпись Error*/
 	return (
-		<div className={styles.formControl + " " + (hasError ? styles.error : " ")}>
+		<div className= {styles.formControl + " " + (hasError ? styles.error : " ")}>
 			<div>
-			<textarea {...input} {...props} />
-			<div/>
+			{props.children}
+			</div>
 			{ hasError && <span>{meta.error}</span>}
-		</div>
 		</div>
 		)
 }
 
-export const Input = ({input, meta, ...props})=>{
-	
-    const hasError = meta.touched && meta.error; /*это приходит из пропсов валилатора,см документацию*/
-    /*если есть ошибка в ответа пропсов,то покажем красный спан надпись Error*/
-	return (
-		<div className={styles.formControl + " " + (hasError ? styles.error : " ")}>
-			<div>
-			<input {...input} {...props} />
-			<div/>
-			{ hasError && <span>{meta.error}</span>}
-		</div>
-		</div>
-		)
+/*убираем дублирующийся код, делаем главную компоненту FormControl и ей передаем чайлдов 
+в Textarea засовываем главную FormControl и передаем ей как чайлда textarea
+*/
+
+export const Textarea = (props) => {/*Textarea является оберткой над FormControl*/
+	const {input, meta, child, ...restProps} = props;
+	return <FormControl {...props}><textarea {...input} {...restProps}/> </FormControl>
+}
+
+export const Input = (props)=> {
+	const {input, meta, child, ...restProps} = props;
+	return <FormControl {...props}><input {...input} {...restProps}/> </FormControl>
 }
