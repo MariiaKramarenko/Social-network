3c300d5d89a5746de2ed041bcf761a98ba12a9e1
Mariia Kramarenko <mariya.kramarenko00@gmail.com>
2020-01-10 18:48:51 +0100

    added function logout to Header.jsx

--- my-app-react/src/components/Header/Header.jsx
+++ my-app-react/src/components/Header/Header.jsx
@@ -9,7 +9,9 @@ const Header = (props) =>{
       	<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRjPhCc0g7DjgtJV7qybb8wPTTWoaJMKhcdkwP7SyZFCLGlimEk" />
         
         <div className={s.loginBlock}>
-        {props.isAuth? props.login : <NavLink to={'login/'}> Login </NavLink> }
+        {props.isAuth 
+        	? <div> props.login  - <button onClick={props.logout}>Log out</button></div>
+        	: <NavLink to={'login/'}> Login </NavLink> }
 
         </div>
       </header>  
