51187c8c6133be7f468e149341fa82b5994bea1e
Mariia Kramarenko <mariya.kramarenko00@gmail.com>
2020-01-28 23:01:47 +0100

    refactoring for idex.js

--- my-app-react/src/index.js
+++ my-app-react/src/index.js
@@ -8,23 +8,11 @@ import './index.css';
 import {Provider} from 'react-redux';
 
 
-let rerenderEntireTree = (state) => {
+
 	ReactDOM.render(
     <Provider store={store}>
-	<App state={state} dispatch={store.dispatch.bind(store)} store={store} 
-	/>
+	<App/>
     </Provider>
 	, document.getElementById('root'));
-}
 
-rerenderEntireTree(store.getState());/*здесь нам не нужен bind() потомучто мы сразу вызываем от имени store за счет постановки скобок()*/
-
-store.subscribe( () => {/*когда изменится стор он вызовет стреочную функцию и в ней мы вызовем ререндер */
-	
-	let state = store.getState();/*берем стейт сами */
-	rerenderEntireTree(state);/*и передаем его ререндеру,так работает редакс! он сам не перадет стейт после его изменения!*/
-
-});/*передаем по коллбеку rerenderEntireTree в stete(там мы вызываем subscribe и получаем rerenderEntireTree для выполнения функций ) */
-
-/*так мы избежали циклической зависимости-flux-архитектура*/
 serviceWorker.unregister();
