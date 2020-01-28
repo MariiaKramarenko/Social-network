6ba02b08d93f84ea1eaba22b91892d6755be2c0f
Mariia Kramarenko <mariya.kramarenko00@gmail.com>
2020-01-28 11:26:13 +0100

    use PureComponent for show how we can hide shouldComponentUpdate in our component (see console with "RENDER YO")

--- my-app-react/src/components/Profile/MyPosts/MyPosts.jsx
+++ my-app-react/src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 import {Field, reduxForm} from 'redux-form';
 import {required, maxLengthCreator} from '../../../utils/validators/validators';
 import {Textarea} from '../../common/FormsControls/FormsControls';
 
-const MyPosts = (props) => {/*константа майпост принимает в пропсы state*/
 
- let postsElements = /*орисовываем (мапим) посты путем преобразования массива posts в jsx элемент*/
-    props.posts.map( p =>
+const maxLength10 = maxLengthCreator(10);/*криетор проверки формы на длину символов*/
+
+const AddNewPostForm = (props) =>{/*выносим форму в отдельную компоненту*/
+ return (
+         <form onSubmit={props.handleSubmit}>
+            <div>
+              <Field validate={[required, maxLength10]} placeholder={"Send your post"} name={"newPostText"} component={Textarea}/>
+            </div>
+            <div>
+              <button className={s.addpost}>Add post</button>
+            </div>
+          </form>
+          )
+}
+/*форма созданная с помощью редакс форм-то есь обернутая в хук редаксФорм*/
+const AddNewPostFormRedux = reduxForm({form:"ProfileAddNewPostForm"}) (AddNewPostForm);/*оборачиваем форму в хок редакс формы*/
+
+
+class MyPosts extends PureComponent {/*классовая PureComponent- содержит в себе метод shoukdComponentUpdate, 
+то есть компонента не требует прописывания этого метода (компонента должна обновиться? сравнение следующих пропсов и стейта снастоящими)компонента майпост
+shouldComponentUpdate(nextProps, nextState){
+   return nextProps != this.props || nextState != this.state;
+}
+
+*/
+
+render(){
+  console.log("RENDER YO");
+
+let postsElements = /*орисовываем (мапим) посты путем преобразования массива posts в jsx элемент*/
+    this.props.posts.map( p =>
       <Post message={p.message} likesCount={p.likesCount}/>
       );
 
 
- let onAddPost = (values) => {/*функция добавления поста на стену*/
-   props.addPost(values.newPostText);/*вызываем коллбек который задиспатчит значение нового поста пришедшее из onSubmit*/
+let onAddPost = (values) => {/*функция добавления поста на стену*/
+   this.props.addPost(values.newPostText);/*вызываем коллбек который задиспатчит значение нового поста пришедшее из onSubmit*/
    
  }
 
@@ -30,21 +58,6 @@ const MyPosts = (props) => {/*константа майпост принимае
   	)
 
 }
-const maxLength10 = maxLengthCreator(10);
-
-const AddNewPostForm = (props) =>{/*выносим форму в отдельную компоненту*/
- return (
-         <form onSubmit={props.handleSubmit}>
-            <div>
-              <Field validate={[required, maxLength10]} placeholder={"Send your post"} name={"newPostText"} component={Textarea}/>
-            </div>
-            <div>
-              <button className={s.addpost}>Add post</button>
-            </div>
-          </form>
-          )
 }
-
-const AddNewPostFormRedux = reduxForm({form:"ProfileAddNewPostForm"}) (AddNewPostForm);/*оборачиваем форму в хок редакс формы*/
 
 export default MyPosts;
