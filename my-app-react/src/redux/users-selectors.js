541261f0561e950ab1f4133a36bfc1547a1de16b
Mariia Kramarenko <mariya.kramarenko00@gmail.com>
2020-02-18 19:29:37 +0100

    /*comments*/

--- my-app-react/src/redux/users-selectors.js
+++ my-app-react/src/redux/users-selectors.js
@@ -1,12 +1,16 @@
 import {createSelector} from "reselect";
-
+//селекторы упрлщают последующие изменения данных то есть 
+//грубо говоря даные кот нужно менять нужно будет изеннить только в одном месте-в селеторе
+//селектор-функция кот принимает стейт и возвращает чтото из него
+//проблема селекторов-частые перевызовы так как mapStateToProps постоянно обновляется
+//для этого на помощь приходит reselect - передаем зависимости на которые будет реагировать селектор если они поменялись и тогда перерисовываться
 export const getUsersAll = (state) =>{/*примитивный селектор*/
 	return state.usersPage.users;
 }
 
 /*селектор,созданный с помощью библиотеки reselect*/
 export const getUsersSuperSelector = createSelector(getUsersAll,(users)=>{/*первый параметр-селектор,кот ипользуется чтобы получить users*/
-    return users.filter(u => true);
+    return users.filter(u => true);//то есть если users изменился тогда срабатывает фильтрация
 })
 
 export const getPageSize = (state) =>{
