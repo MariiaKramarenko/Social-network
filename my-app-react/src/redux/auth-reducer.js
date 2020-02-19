26a58e991b939d4f3ac1e03e4bad846fe75bd224
Mariia Kramarenko <mariya.kramarenko00@gmail.com>
2020-02-19 09:54:25 +0100

    refactoring for auth-reducer (used await asunc instead then.(response =>)

--- my-app-react/src/redux/auth-reducer.js
+++ my-app-react/src/redux/auth-reducer.js
@@ -3,7 +3,7 @@ import {stopSubmit} from 'redux-form';
 
 //редьюсер авторизации в нашем приложении
 
-const SET_USER_DATA = 'SET_USER_DATA';
+const SET_USER_DATA = 'my-first-network/auth/SET_USER_DATA';//уникализируем путь для срабатывания action
 
 
 let initialState = {//начальный стейт с данными для авторизации в приложении
@@ -31,38 +31,46 @@ const authReducer = (state = initialState, action) => {//компонента-р
 
 export const setAuthUserData = (userId,email,login,isAuth) =>({type:SET_USER_DATA, payload: {userId,email,login,isAuth} })/*экшнкриэйтор для отправки сообщения*/
 
-export const getAuthUserData = () => (dispatch) => {/*санк-криейтор для получения данных о залогиненном юзере*/
-   authAPI.me()/*запрос на сервер на me*/
-      .then(response=> {/*в ответе (респонсе) от сервера сидят даннные*/
-          if(response.data.resultCode === 0 ){/*если от сервера пришел ответ 0 значит успешен запрос*/
-            let {id,email,login} = response.data.data;/*а значит что мы  берем все данные из респонса сервера и диспатчим(вызываем)их установку*/
-            dispatch(setAuthUserData(id,email,login,true));/*напомню что setAuthUserData записывает в state пришедшие ему данные юзера (мы логинимся в апишке и там в куке они сидят) */
-          }
-        });
 
-}
 
-export const login = (email, password, rememberMe) => (dispatch) => {/*санк-криейтор для получения данных для входа(логина)*/
-   authAPI.login(email, password, rememberMe)/*запрос на сервер на login и передаем данные для логинизации*/
-      .then(response=> {/*в ответе (респонсе) от сервера сидят даннные*/
-          if(response.data.resultCode === 0 ){/*если от сервера пришел ответ 0 значит успешен запрос*/
-             dispatch(getAuthUserData())/*диспатчим getAuthUserData кот. получает данные*/
-          } else{/*обработка ввода неверных данных в форму логина*/
-             let messages = response.data.messages.length > 0 ? response.data.messages[0] : "Some error";/*берем значение строчное ошибки из данных от сервера,там они сидят в []*/
-           /*берем первое значение сообщения обошибке(приходит от сервера и на всякий случай ставим свою надпись об ошибке*/
-           dispatch(stopSubmit("login", {_error: messages}));/*первое значение-какую форму мы останавливаем на проверку(_error-значение для всех полей общее),второй парам- объект с проверяемыми значениями(поля формы)*/
-          }
-        });
+
+
+export const getAuthUserData = () => async (dispatch) => {/*санк-криейтор для получения данных о залогиненном юзере*/
+   /*помним что как результат работы асинхронной функции вернется промис*/
+   let response = await authAPI.me();/*запрос на сервер на me, ожидаем промис с пом await*/
+     /*присваиваем переменной rensponse тот response кот вернул нам await*/
+    if(response.data.resultCode === 0 ){/*если от сервера пришел ответ 0 значит успешен запрос*/
+      let {id,email,login} = response.data.data;/*а значит что мы  берем все данные из респонса сервера и диспатчим(вызываем)их установку*/
+    dispatch(setAuthUserData(id,email,login,true));/*напомню что setAuthUserData записывает в state пришедшие ему данные юзера (мы логинимся в апишке и там в куке они сидят) */
+    }
 }
 
 
-export const logout = () => (dispatch) => {/*санк-криейтор для вылогинизации из соцсети*/
-   authAPI.logout()/*запрос на сервер на logout */
-      .then(response=> {/*в ответе (респонсе) от сервера сидят даннные*/
-          if(response.data.resultCode === 0 ){/*если от сервера пришел ответ 0 значит успешен запрос*/
-            dispatch(setAuthUserData(null,null,null,false));/*зануляем все поля с данныеми когда вылогиниваемся*/
-          }
-        });
+
+
+
+export const login = (email, password, rememberMe) => async (dispatch) => {/*санк-криейтор для получения данных для входа(логина)*/
+   /*!!!!!!!!!!!ВАЖНО: функция кот содержит await должна быть async!!!!!!!!!!*/
+   let response = await authAPI.login(email, password, rememberMe);/*запрос на сервер на login и передаем данные для логинизации*/
+    /*присваиваем переменной rensponse тот response кот вернул нам await*/
+    if(response.data.resultCode === 0 ){/*если от сервера пришел ответ 0 значит успешен запрос*/
+      dispatch(getAuthUserData())/*диспатчим getAuthUserData кот. получает данные*/
+    } else{/*обработка ввода неверных данных в форму логина*/
+      let messages = response.data.messages.length > 0 ? response.data.messages[0] : "Some error";/*берем значение строчное ошибки из данных от сервера,там они сидят в []*/
+    /*берем первое значение сообщения обошибке(приходит от сервера и на всякий случай ставим свою надпись об ошибке*/
+    dispatch(stopSubmit("login", {_error: messages}));/*первое значение-какую форму мы останавливаем на проверку(_error-значение для всех полей общее),второй парам- объект с проверяемыми значениями(поля формы)*/
+    }
+
+}
+
+
+export const logout = () => async (dispatch) => {/*санк-криейтор для вылогинизации из соцсети*/
+   let response = await authAPI.logout();/*запрос на сервер на logout и запись зарезолвенного прописа в response*/
+/*в ответе (респонсе) от сервера сидят даннные*/
+    if(response.data.resultCode === 0 ){/*если от сервера пришел ответ 0 значит успешен запрос*/
+        dispatch(setAuthUserData(null,null,null,false));/*зануляем все поля с данныеми когда вылогиниваемся*/
+    }
+
 }
 
 
