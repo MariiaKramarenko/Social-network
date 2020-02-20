c18a9e6a04ae876abf9f362698811c792bce79f4
Mariia Kramarenko <mariya.kramarenko00@gmail.com>
2020-02-20 13:44:26 +0100

    refactoring for Paginator (whow only 10 pages, added buttons PREV -NEXT for changing pages)

--- my-app-react/src/components/Users/Users.jsx
+++ my-app-react/src/components/Users/Users.jsx
@@ -8,8 +8,7 @@ import User from './User';
 
 let Users = ({currentPage, totalUsersCount, pageSize, onPageChanged, users, ...props}) => {
     return <div>
-        <Paginator currentPage={currentPage} onPageChanged={onPageChanged}
-                   totalUsersCount={totalUsersCount} pageSize={pageSize}/>
+        
         <div>
              {
                 users.map(u => <User user={u}
@@ -21,6 +20,8 @@ let Users = ({currentPage, totalUsersCount, pageSize, onPageChanged, users, ...p
                 )
             }
         </div>
+        <Paginator currentPage={currentPage} onPageChanged={onPageChanged}
+                   totalItemsCount={totalUsersCount} pageSize={pageSize}/>
             </div>
 }
 export default Users;
--- my-app-react/src/components/common/Paginator/Paginator.jsx
+++ my-app-react/src/components/common/Paginator/Paginator.jsx
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from "./Paginator.module.css";
+import cn from "classnames";
 
-let Paginator = ({currentPage, onPageChanged, totalUsersCount, pageSize  }) => {
-    let pagesCount = Math.ceil(totalUsersCount / pageSize);
 
-    let pages = [];
-    for (let i = 1; i <= pagesCount; i++) {
+let Paginator = ({totalItemsCount, pageSize, currentPage, onPageChanged, portionSize = 10}) => {
+
+    let pagesCount = Math.ceil(totalItemsCount / pageSize);//переменная с количеством страниц пользователей
+
+    let pages = [];//массив с пользователями
+    for (let i = 1; i <= pagesCount; i++) {//вывод пользователей через цикл for (увеличиваем каждую последующую страницу и пушим ее)
         pages.push(i);
     }
 
-    return <div>
-            {pages.map(p => {
-                return <span className={currentPage === p && styles.selectedPage}
-                             onClick={(e) => {
+    let portionCount = Math.ceil(pagesCount / portionSize);//рассчет порции кот будет видна нам в виде пагинации
+    let [portionNumber, setPortionNumber] = useState(1);//используем хук useState чтобы portionNumber - номер порции
+    let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;//определяем значение левой кнопки порции страниц
+    let rightPortionPageNumber = portionNumber * portionSize;//определяем значение правой кнопки порции страниц
+
+
+    return <div className={styles.paginator}>
+        { portionNumber > 1 && //если номер порции больше 1 тогда показываем кнопку PREV -которая переключает на предыдущую порцию
+        <button onClick={() => { setPortionNumber(portionNumber - 1) }}>PREV</button> }
+
+            {pages //выводим номера страничек с пользователями
+                .filter(p => p >= leftPortionPageNumber && p<=rightPortionPageNumber)//фильтруем левый и правы номера страниц
+                .map((p) => {//если номер больше либо равен числу левого номера порции и меньше или равен правого числа номера порции
+                return <span className={ cn({//тогда возвращаем span кот содержит стили и если фильтрация вернула true то 
+                    [styles.selectedPage]: currentPage === p//текущая страница равна той что фильтруется
+                }, styles.pageNumber) }
+                             key={p}//применяем обязательно key
+                             onClick={(e) => {//добавляем функцию кот по клику показывает пользователей текущей страницы
                                  onPageChanged(p);
                              }}>{p}</span>
             })}
-        </div>
+                { portionCount > portionNumber && //когда  тогда показываем кнопку NEXT-кот переключает на следующую порцию с пользователями
+    <button onClick={() => { setPortionNumber(portionNumber + 1) }}>NEXT</button> }
+
+
+    </div>
+
 }
 
 export default Paginator; 
