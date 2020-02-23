8cc5961b2d80bc26174b29ec9674edd8e02bce4f
Mariia Kramarenko <mariya.kramarenko00@gmail.com>
2020-02-24 00:19:28 +0100

    Updates

---
+++ precache-manifest.130139f0d1ca1956199e29d69e30ef4c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eab314cd9a554f91dccba528f2b7e29",
+    "url": "/Social-network/index.html"
+  },
+  {
+    "revision": "c96aa70f64a65eb8fae3",
+    "url": "/Social-network/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "508f429a5b5de4252b2c",
+    "url": "/Social-network/static/css/3.4e1ebaf9.chunk.css"
+  },
+  {
+    "revision": "fe2e5602b3b73ef8ec72",
+    "url": "/Social-network/static/css/4.ee2ab2c0.chunk.css"
+  },
+  {
+    "revision": "e7758deda5b8d16af911",
+    "url": "/Social-network/static/css/main.3004fa7a.chunk.css"
+  },
+  {
+    "revision": "c96aa70f64a65eb8fae3",
+    "url": "/Social-network/static/js/2.384bcd30.chunk.js"
+  },
+  {
+    "revision": "508f429a5b5de4252b2c",
+    "url": "/Social-network/static/js/3.7e27f536.chunk.js"
+  },
+  {
+    "revision": "fe2e5602b3b73ef8ec72",
+    "url": "/Social-network/static/js/4.6704272f.chunk.js"
+  },
+  {
+    "revision": "e7758deda5b8d16af911",
+    "url": "/Social-network/static/js/main.de8c7906.chunk.js"
+  },
+  {
+    "revision": "4cfa4e2c80706aa4394e",
+    "url": "/Social-network/static/js/runtime-main.b2385c82.js"
+  },
+  {
+    "revision": "2b1d93cf7469360e98a6f7f4abf2b6ea",
+    "url": "/Social-network/static/media/User.2b1d93cf.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Social-network/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "22f289193873ba2266a9ead25964f071",
+    "url": "/Social-network/static/media/preloader.22f28919.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Social-network/precache-manifest.ad0ed1642e1d627db92de37d101f38e3.js"
+  "/Social-network/precache-manifest.130139f0d1ca1956199e29d69e30ef4c.js"
 );
 
 self.addEventListener('message', (event) => {
